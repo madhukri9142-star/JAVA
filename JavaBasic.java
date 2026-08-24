bc0b57d43f1a14e15dca9b029fa981970599eae8
madhukri9142-star <madhukri9142@gmail.com>
2026-08-24 11:44:26 +0530

    Added JavaBasic.java

---
+++ JavaBasic.java
@@ -0,0 +1,7 @@
+public class JavaBasic{
+    public static void main(String[] args) {
+  System.out.print("Hello Madhu\n");//hum println k jagah p backslash n bhi use kar sakte hai
+  System.out.println("Hello Madhu");
+  System.out.println("Hello Madhu");//println use karne p ik line kh space de deta hai 
+  }
+}
