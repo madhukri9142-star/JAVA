4bf0795a2c81c46f2fae7eccac0c17f71c3f6bc6
madhukri9142-star <madhukri9142@gmail.com>
2026-08-24 22:17:43 +0530

    Added Datatype.java

---
+++ Datatype.java
@@ -0,0 +1,10 @@
+public class Datatype {
+    public static void main(String [] args){
+        byte b = 8;
+        System.out.println(b);
+        char ch = 'a';
+        System.out.println(ch);
+
+    }
+    
+}
